46069d55f90be47c346df30eda25eace04b25594
kumarvipin1983 <kumar.ricky@gmail.com>
2021-11-21 22:33:25 +0530

    Initial Commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.in28minutes.rest.webservices.restfulwebservices.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.in28minutes.rest.webservices.restfulwebservices.exception.UserNotFoundException;
+import com.in28minutes.rest.webservices.restfulwebservices.model.User;
+import com.in28minutes.rest.webservices.restfulwebservices.service.UserDaoService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDaoService service;
+
+	// GET /users
+	// retrieveAllUser
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	// GET /users/{id}
+	// retrieveUser(int id)
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) throws UserNotFoundException {
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("Id = " + id, null, false, false);
+		}
+		return user;
+	}
+
+	// input - details of user
+	// output - Created and return the created URI
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/UserNotFoundException.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
